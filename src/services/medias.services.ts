e58119558ac69f8f317461d745a2a4d41e2de4f3
namdh03 <duonghoangnam503@gmail.com>
2024-05-12 13:03:59 +0700

    feature: upload video

--- src/services/medias.services.ts
+++ src/services/medias.services.ts
@@ -3,10 +3,10 @@ import fs from 'fs'
 import path from 'path'
 import sharp from 'sharp'
 import { isProduction } from '~/constants/configs'
-import { UPLOAD_DIR } from '~/constants/dir'
+import { UPLOAD_IMAGE_DIR } from '~/constants/dir'
 import { MediaType } from '~/constants/enums'
 import { Media } from '~/models/Other'
-import { getFileName, handleUploadImage } from '~/utils/file'
+import { getFileName, handleUploadImage, handleUploadVideo } from '~/utils/file'
 
 class MediasService {
   async uploadImage(req: Request) {
@@ -14,19 +14,31 @@ class MediasService {
     const result: Media[] = await Promise.all(
       files.map(async (file) => {
         const newName = getFileName(file.newFilename)
-        const newPath = path.resolve(UPLOAD_DIR, `${newName}.jpg`)
+        const newPath = path.resolve(UPLOAD_IMAGE_DIR, `${newName}.jpg`)
 
         await sharp(file.filepath).jpeg().toFile(newPath)
         fs.unlinkSync(file.filepath)
 
         return {
           url: isProduction
-            ? `${process.env.HOST}/static/${newName}.jpg`
-            : `http://localhost:${process.env.PORT}/static/${newName}.jpg`,
+            ? `${process.env.HOST}/static/image/${newName}.jpg`
+            : `http://localhost:${process.env.PORT}/static/image/${newName}.jpg`,
           type: MediaType.Image
         }
       })
     )
+
+    return result
+  }
+
+  async uploadVideo(req: Request) {
+    const files = await handleUploadVideo(req)
+    const result: Media[] = files.map((file) => ({
+      url: isProduction
+        ? `${process.env.HOST}/static/video/${file.newFilename}`
+        : `http://localhost:${process.env.PORT}/static/video/${file.newFilename}`,
+      type: MediaType.Video
+    }))
 
     return result
   }
--- src/utils/file.ts
+++ src/utils/file.ts
@@ -2,20 +2,22 @@ import { Request } from 'express'
 import { File } from 'formidable'
 import fs from 'fs'
 import path from 'path'
-import { UPLOAD_TEMP_DIR } from '~/constants/dir'
+import { UPLOAD_IMAGE_TEMP_DIR, UPLOAD_VIDEO_DIR, UPLOAD_VIDEO_TEMP_DIR } from '~/constants/dir'
 
 export const initFolder = () => {
-  if (!fs.existsSync(UPLOAD_TEMP_DIR)) {
-    fs.mkdirSync(UPLOAD_TEMP_DIR, {
-      recursive: true // Tạo folder cha nếu chưa tồn tại
-    })
-  }
+  ;[UPLOAD_IMAGE_TEMP_DIR, UPLOAD_VIDEO_TEMP_DIR].forEach((dir) => {
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, {
+        recursive: true // Tạo folder cha nếu chưa tồn tại
+      })
+    }
+  })
 }
 
 export const handleUploadImage = async (req: Request) => {
   const formidable = (await import('formidable')).default
   const form = formidable({
-    uploadDir: UPLOAD_TEMP_DIR,
+    uploadDir: UPLOAD_IMAGE_TEMP_DIR,
     maxFiles: 4,
     maxFileSize: 300 * 1024, // 300KB
     maxTotalFileSize: 300 * 1024 * 4, // 300KB * 4
@@ -46,10 +48,48 @@ export const handleUploadImage = async (req: Request) => {
   })
 }
 
+export const handleUploadVideo = async (req: Request) => {
+  const formidable = (await import('formidable')).default
+  const form = formidable({
+    uploadDir: UPLOAD_VIDEO_DIR,
+    maxFiles: 1,
+    maxFileSize: 50 * 1024 * 1024, // 50MB
+    filter: ({ name, mimetype }) => {
+      return true
+    }
+  })
+
+  return new Promise<File[]>((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        return reject(err)
+      }
+
+      if (!files.video) {
+        return reject(new Error('File is empty'))
+      }
+
+      const videos = files.video as File[]
+
+      videos.forEach((video) => {
+        const extension = getExtension(video.originalFilename as string)
+
+        fs.renameSync(video.filepath, video.filepath + '.' + extension)
+        video.newFilename = video.newFilename + '.' + extension
+      })
+
+      resolve(files.video as File[])
+    })
+  })
+}
+
 export const getFileName = (name: string) => {
   const nameArr = name.split('.')
-
   nameArr.pop()
-
   return nameArr.join('')
+}
+
+export const getExtension = (name: string) => {
+  const nameArr = name.split('.')
+  return nameArr[nameArr.length - 1]
 }
