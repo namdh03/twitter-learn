9366ff2b4a03f24c2e6f8d4d0d19028f4742d8c9
namdh03 <duonghoangnam503@gmail.com>
2024-07-17 10:51:39 +0700

    feature: add middleware socket

--- src/index.ts
+++ src/index.ts
@@ -40,18 +40,20 @@ const users: {
 } = {}
 
 io.use(async (socket, next) => {
-  console.log(socket.id, socket.handshake.auth)
   const { Authorization } = socket.handshake.auth
   const access_token = Authorization?.split(' ')[1]
+
   try {
     const decoded_authorization = await verifyAccessToken(access_token)
     const { verify } = decoded_authorization as TokenPayload
+
     if (verify !== UserVerifyStatus.Verified) {
       throw new ErrorWithStatus({
         message: USERS_MESSAGES.USER_IS_NOT_VERIFIED,
         status: HTTP_STATUS.FORBIDDEN
       })
     }
+
     // Truyền decoded_authorization vào socket để sử dụng ở các middleware khác
     socket.handshake.auth.decoded_authorization = decoded_authorization
     socket.handshake.auth.access_token = access_token
@@ -64,14 +66,16 @@ io.use(async (socket, next) => {
     })
   }
 })
+
 io.on('connection', (socket) => {
   console.log(`user ${socket.id} connected`)
+
   const { user_id } = socket.handshake.auth.decoded_authorization as TokenPayload
-  users[user_id] = {
-    socket_id: socket.id
-  }
-  socket.use(async (packet, next) => {
+  users[user_id] = { socket_id: socket.id }
+
+  socket.use(async (_, next) => {
     const { access_token } = socket.handshake.auth
+
     try {
       await verifyAccessToken(access_token)
       next()
@@ -85,6 +89,7 @@ io.on('connection', (socket) => {
       socket.disconnect()
     }
   })
+
   socket.on('send_message', async (data) => {
     const { receiver_id, sender_id, content } = data.payload
     const receiver_socket_id = users[receiver_id]?.socket_id
@@ -94,16 +99,20 @@ io.on('connection', (socket) => {
       content: content
     })
     const result = await databaseService.conversations.insertOne(conversation)
+
     conversation._id = result.insertedId
+
     socket.emit('send_message_success', {
       payload: conversation
     })
+
     if (receiver_socket_id) {
       socket.to(receiver_socket_id).emit('receive_message', {
         payload: conversation
       })
     }
   })
+
   socket.on('disconnect', () => {
     delete users[user_id]
     console.log(`user ${socket.id} disconnected`)
