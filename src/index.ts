3aa2d0d2b83481b35870291c17374f506fa181f1
namdh03 <duonghoangnam503@gmail.com>
2024-07-17 11:00:23 +0700

    refactor: socket server

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import { createServer } from 'http'
-import { Server } from 'socket.io'
 
 import databaseService from './services/database.services'
 import { defaultErrorHandler } from './middlewares/error.middlewares'
@@ -14,110 +13,12 @@ import tweetsRouter from './routes/tweets.routes'
 import bookmarksRouter from './routes/bookmarks.routes'
 import likesRouter from './routes/likes.routes'
 import searchRouter from './routes/search.routes'
-import Conversation from './models/schemas/Conversation.schema'
 import conversationsRouter from './routes/conversations.routes'
-import { ObjectId } from 'mongodb'
-import { verifyAccessToken } from './utils/commons'
-import { TokenPayload } from './models/requests/User.requests'
-import { UserVerifyStatus } from './constants/enums'
-import { ErrorWithStatus } from './models/Errors'
-import HTTP_STATUS from './constants/httpStatus'
-import { USERS_MESSAGES } from './constants/messages'
+import initSocket from './utils/socket'
 
 const app = express()
 const httpServer = createServer(app)
 const port = process.env.PORT || 4000
-const io = new Server(httpServer, {
-  cors: {
-    origin: 'http://localhost:3000'
-  }
-})
-
-const users: {
-  [key: string]: {
-    socket_id: string
-  }
-} = {}
-
-io.use(async (socket, next) => {
-  const { Authorization } = socket.handshake.auth
-  const access_token = Authorization?.split(' ')[1]
-
-  try {
-    const decoded_authorization = await verifyAccessToken(access_token)
-    const { verify } = decoded_authorization as TokenPayload
-
-    if (verify !== UserVerifyStatus.Verified) {
-      throw new ErrorWithStatus({
-        message: USERS_MESSAGES.USER_IS_NOT_VERIFIED,
-        status: HTTP_STATUS.FORBIDDEN
-      })
-    }
-
-    // Truyền decoded_authorization vào socket để sử dụng ở các middleware khác
-    socket.handshake.auth.decoded_authorization = decoded_authorization
-    socket.handshake.auth.access_token = access_token
-    next()
-  } catch (error) {
-    next({
-      message: 'Unauthorized',
-      name: 'UnauthorizedError',
-      data: error
-    })
-  }
-})
-
-io.on('connection', (socket) => {
-  console.log(`user ${socket.id} connected`)
-
-  const { user_id } = socket.handshake.auth.decoded_authorization as TokenPayload
-  users[user_id] = { socket_id: socket.id }
-
-  socket.use(async (_, next) => {
-    const { access_token } = socket.handshake.auth
-
-    try {
-      await verifyAccessToken(access_token)
-      next()
-    } catch (error) {
-      next(new Error('Unauthorized'))
-    }
-  })
-
-  socket.on('error', (error) => {
-    if (error.message === 'Unauthorized') {
-      socket.disconnect()
-    }
-  })
-
-  socket.on('send_message', async (data) => {
-    const { receiver_id, sender_id, content } = data.payload
-    const receiver_socket_id = users[receiver_id]?.socket_id
-    const conversation = new Conversation({
-      sender_id: new ObjectId(sender_id as string),
-      receiver_id: new ObjectId(receiver_id as string),
-      content: content
-    })
-    const result = await databaseService.conversations.insertOne(conversation)
-
-    conversation._id = result.insertedId
-
-    socket.emit('send_message_success', {
-      payload: conversation
-    })
-
-    if (receiver_socket_id) {
-      socket.to(receiver_socket_id).emit('receive_message', {
-        payload: conversation
-      })
-    }
-  })
-
-  socket.on('disconnect', () => {
-    delete users[user_id]
-    console.log(`user ${socket.id} disconnected`)
-  })
-})
 
 databaseService.connect().then(() => {
   databaseService.indexUsers()
@@ -141,6 +42,8 @@ app.use('/search', searchRouter)
 app.use('/static', staticsRouter)
 app.use('/conversations', conversationsRouter)
 app.use(defaultErrorHandler)
+
+initSocket(httpServer)
 
 httpServer.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
